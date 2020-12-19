24bfeeca15c1eac77c03eea33f8776a2090630d6
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-12-19 22:01:54 +0800

    show list product

--- src/Product/List.js
+++ src/Product/List.js
@@ -1,9 +1,47 @@
+import Axios from "axios";
 import React from "react";
 
 export default function List() {
+    const [products, setProducts] = React.useState([]);
+
+    React.useEffect(() => {
+        Axios.get("http://localhost:3000/products")
+            .then((res) => {
+                const { status, message, data } = res.data;
+
+                if (status === "success") {
+                    setProducts(data);
+                } else {
+                    alert(message);
+                }
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <div>
-            <h2>List product page</h2>
+            <h2>List product {products.length}</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products &&
+                        products.map((product, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td className="center">{product.name}</td>
+                                    <td className="center">{product.price}</td>
+                                    <td className="center">{product.stock}</td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </table>
         </div>
     );
 }
